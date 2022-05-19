43aaf97657c9b83d01c1fceba9c6f957359ce135
Abby Luce <abby.c.luce@gmail.com>
2022-05-19 15:28:09 -0600

    Initial commit

--- src/main.js
+++ src/main.js
@@ -1,7 +1,26 @@
-// Create variables targetting the relevant DOM elements here 👇
+// // Create variables targetting the relevant DOM elements here 👇
+var randomTitle = document.querySelector(".cover-title");
+randomTitle.innerText = Cover.title
+
+var randomImage = document.querySelector(".cover-image");
+randomImage.innerHTML = Cover.coverImgSrc
+
+var randomTagline1 = document.querySelector(".tagline-1");
+randomTagline1.innerText = Cover.descriptor1
+
+var randomTagline2 = document.querySelector(".tagline-2");
+randomTagline2.innerText = Cover.descriptor2
+
+var newRandomButton = document.querySelector(".random-cover-button");
+
+var newSaveButton = document.querySelector(".save-cover-button");
+
+var newViewButton = document.querySelector(".view-saved-button");
+
+var newMakeButton = document.querySelector(".make-new-button");
 
 
-// We've provided a few variables below
+// // We've provided a few variables below
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
@@ -14,6 +33,7 @@ var currentCover;
 
 
 // We've provided one function to get you started
-function getRandomIndex(array) {
-  return Math.floor(Math.random() * array.length);
+function getRandomIndex(covers) {
+  return Math.floor(Math.random() * covers.length);
 }
+console.log(getRandomIndex());
