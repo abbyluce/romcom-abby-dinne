2aa883432a1951db5b26032149a4b37792280355
Dinne Kopelevich <dinne22@hotmail.com>
2022-05-19 21:11:49 -0600

    Add Iteration 0

--- src/main.js
+++ src/main.js
@@ -1,39 +1,46 @@
 // // Create variables targetting the relevant DOM elements here 👇
 var randomTitle = document.querySelector(".cover-title");
-randomTitle.innerText = Cover.title
-
 var randomImage = document.querySelector(".cover-image");
-randomImage.innerHTML = Cover.coverImgSrc
-
 var randomTagline1 = document.querySelector(".tagline-1");
-randomTagline1.innerText = Cover.descriptor1
-
 var randomTagline2 = document.querySelector(".tagline-2");
-randomTagline2.innerText = Cover.descriptor2
 
 var newRandomButton = document.querySelector(".random-cover-button");
-
 var newSaveButton = document.querySelector(".save-cover-button");
-
 var newViewButton = document.querySelector(".view-saved-button");
-
 var newMakeButton = document.querySelector(".make-new-button");
 
 
 // // We've provided a few variables below
 var savedCovers = [
-  new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
+  new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunrises", "stabs")
 ];
 var currentCover;
 
+// var viewForm = document.createElement('.view form-view hidden');
 // Add your event listeners here 👇
+newRandomButton.addEventListener('click', clickRandomButton);
+newMakeButton.addEventListener('click', clickMakeButton);
 
+function clickRandomButton() {
+  randomTitle.innerText = changeCover(titles);
+  randomImage.src = changeCover(covers);
+  randomTagline1.innerText = changeCover(descriptors);
+  randomTagline2.innerText = changeCover(descriptors);
+};
+
+// function clickMakeButton(e) {
+//   e.preventDefault();
+//   viewForm.classList.toggle('visible');
+// };
 
 // Create your event handlers and other functions here 👇
-
+function changeCover(arrays) {
+  var titleNumber = getRandomIndex(arrays);
+  var titleName = arrays[titleNumber];
+  return titleName
+}
 
 // We've provided one function to get you started
-function getRandomIndex(covers) {
-  return Math.floor(Math.random() * covers.length);
-}
-console.log(getRandomIndex());
+function getRandomIndex(banana) {
+  return Math.floor(Math.random() * banana.length);
+};
